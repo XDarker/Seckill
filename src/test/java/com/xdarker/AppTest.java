946ab91440c1b532df820b6b12ab42d5db30d054
XDarker <xiashengjianah@163.com>
2018-08-21 21:38:39 +0800

    init

---
+++ src/test/java/com/xdarker/AppTest.java
@@ -0,0 +1,20 @@
+package com.xdarker;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple SeckillApplication.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
