4eb81881d2d37bfdf98b1c1bb3dcd9c0c4c15031
XDarker <xiashengjianah@163.com>
2018-08-27 15:50:13 +0800

    限制次数

---
+++ src/main/java/com/xdarker/cache/AccessKey.java
@@ -0,0 +1,11 @@
+package com.xdarker.cache;
+
+/**
+ * Created by XDarker
+ * 2018/8/25 20:38
+ */
+public class AccessKey {
+
+    public static final int ACCESSTIME = 5;
+    public static final String ACCESS = "access";
+}
