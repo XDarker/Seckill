704d1c5d6f23e0eb42bc34a165650bc82374170d
XDarker <xiashengjianah@163.com>
2018-08-27 15:50:36 +0800

    商品前缀

---
+++ src/main/java/com/xdarker/cache/GoodsKey.java
@@ -0,0 +1,17 @@
+package com.xdarker.cache;
+
+/**
+ * Created by XDarker
+ * 2018/8/26 13:14
+ */
+public class GoodsKey {
+
+//	private GoodsKey(int expireSeconds, String prefix) {
+//		super(expireSeconds, prefix);
+//	}
+//	public static GoodsKey getGoodsList = new GoodsKey(60, "gl");
+//	public static GoodsKey getGoodsDetail = new GoodsKey(60, "gd");
+//	public static GoodsKey getSecKillGoodsStock= new GoodsKey(0, "gs");
+
+    public static final String SECKILLGOODSSTOCK = "GoodsStock";
+}
