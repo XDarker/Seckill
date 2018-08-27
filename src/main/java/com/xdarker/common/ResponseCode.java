08ed9c895b413ab4b9d98ba599a94249c1832633
XDarker <xiashengjianah@163.com>
2018-08-27 15:51:49 +0800

    请求响应码

---
+++ src/main/java/com/xdarker/common/ResponseCode.java
@@ -0,0 +1,42 @@
+package com.xdarker.common;
+
+/**
+ * 响应码枚举类
+ * Created by XDarker
+ * 2018/8/25 20:43
+ */
+public enum ResponseCode {
+
+    SUCCESS(0,"SUCCESS"),
+    ERROR(1,"ERROR"),
+    NEED_LOGIN(10,"NEED_LOGIN"),
+    ILLEGAL_ARGUMENT(2,"ILLEGAL_ARGUMENT"),
+
+
+    //登录模块 5002XX
+    SESSION_ERROR(500210, "Session不存在或者已经失效"),
+    PASSWORD_EMPTY(500211, "登录密码不能为空"),
+    MOBILE_EMPTY(500212, "手机号不能为空"),
+    MOBILE_ERROR(500213, "手机号格式错误"),
+    MOBILE_NOT_EXIST(500214, "手机号不存在"),
+    PASSWORD_ERROR(500215, "密码错误"),
+
+    ACCESS_LIMIT(000000,"访问太频繁..");
+
+    private final int code;
+    private final String desc;
+
+
+    ResponseCode(int code,String desc){
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public int getCode(){
+        return code;
+    }
+    public String getDesc(){
+        return desc;
+    }
+
+}
